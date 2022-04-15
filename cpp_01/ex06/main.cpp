2b9f00a761d5ed169d2160b9b7975e123925fe7b
Jaekpark <xhxkf123@gmail.com>
2022-04-15 22:31:04 +0900

    Fix ex05 ex06 Fix log message

--- cpp_01/ex06/main.cpp
+++ cpp_01/ex06/main.cpp
@@ -1,11 +1,12 @@
 #include "Harl.hpp"
 
-int main(void) {
+int main(int argc, char **argv) {
 	Harl harl;
 
-	harl.complain("INFO");
-	harl.complain("DEBUG");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
+	if (argc != 2) {
+		std::cerr << argv[0] << ": Try " << argv[0] << " [Level]" << std::endl;
+		return 1;
+	}
+	harl.complain(argv[1]);
 	return 0;
 }
